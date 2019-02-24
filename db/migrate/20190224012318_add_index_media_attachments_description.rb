ac24fa41a33566217aa2fa7b7378d24b21e97b5f
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-02-24 10:30:10 +0900

    #43

--- db/migrate/20190224012318_add_index_media_attachments_description.rb
+++ db/migrate/20190224012318_add_index_media_attachments_description.rb
@@ -2,6 +2,6 @@ class AddIndexMediaAttachmentsDescription < ActiveRecord::Migration[5.2]
   disable_ddl_transaction!
 
   def change
-    add_index :media_attachments, :description, algorithm: :concurrently
+    add_index :media_attachments, :description, :length => 8192, algorithm: :concurrently
   end
 end
--- db/migrate/20190224012333_add_index_statuses_text.rb
+++ db/migrate/20190224012333_add_index_statuses_text.rb
@@ -2,6 +2,7 @@ class AddIndexStatusesText < ActiveRecord::Migration[5.2]
   disable_ddl_transaction!
 
   def change
-    add_index :statuses, :text, algorithm: :concurrently
+    add_index :statuses, :text, :length => 8192, algorithm: :concurrently
   end
 end
+
