f6647053842386a13fe7869dfaee603676f78e19
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-03-21 06:46:14 +0900

    #52

--- db/migrate/20190224012318_add_index_media_attachments_description.rb
+++
@@ -1,7 +0,0 @@
-class AddIndexMediaAttachmentsDescription < ActiveRecord::Migration[5.2]
-  disable_ddl_transaction!
-
-  def change
-    add_index :media_attachments, :description, :length => 8192, algorithm: :concurrently
-  end
-end
--- db/migrate/20190224012333_add_index_statuses_text.rb
+++
@@ -1,8 +0,0 @@
-class AddIndexStatusesText < ActiveRecord::Migration[5.2]
-  disable_ddl_transaction!
-
-  def change
-    add_index :statuses, :text, :length => 8192, algorithm: :concurrently
-  end
-end
-
