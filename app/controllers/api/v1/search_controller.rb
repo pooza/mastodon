7158f6eb07a6634d70faf84972e51e395f6cd091
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-06-13 10:18:24 +0900

    #70

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -3,7 +3,7 @@
 class Api::V1::SearchController < Api::BaseController
   include Authorization
 
-  RESULTS_LIMIT = (ENV['ES_RESULTS_LIMIT'].to_i || 20)
+  RESULTS_LIMIT = (ENV['ES_RESULTS_LIMIT'] || '20').to_i
 
   before_action -> { doorkeeper_authorize! :read, :'read:search' }
   before_action :require_user!
