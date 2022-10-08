165d95ee02ee8d8bcb601b9e8442510f8e3845c4
pooza <tkoishi@b-shock.co.jp>
2022-10-08 19:41:47 +0900

    #737

--- app/controllers/api/v2/search_controller.rb
+++ app/controllers/api/v2/search_controller.rb
@@ -5,8 +5,7 @@ class Api::V2::SearchController < Api::BaseController
 
   RESULTS_LIMIT = 20
 
-  before_action -> { doorkeeper_authorize! :read, :'read:search' }
-  before_action :require_user!
+  before_action -> { authorize_if_got_token! :read, :'read:search' }
 
   def index
     @search = Search.new(search_results)
@@ -23,12 +22,12 @@ class Api::V2::SearchController < Api::BaseController
     SearchService.new.call(
       params[:q],
       current_account,
-      limit_param(params[:limit].to_i),
-      search_params.merge(resolve: truthy_param?(:resolve), exclude_unreviewed: truthy_param?(:exclude_unreviewed))
+      limit_param(RESULTS_LIMIT),
+      search_params.merge(resolve: user_signed_in? ? truthy_param?(:resolve) : false, exclude_unreviewed: truthy_param?(:exclude_unreviewed))
     )
   end
 
   def search_params
-    params.permit(:type, :offset, :min_id, :max_id, :account_id, :limit)
+    params.permit(:type, :offset, :min_id, :max_id, :account_id)
   end
 end
