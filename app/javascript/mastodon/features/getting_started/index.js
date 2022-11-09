56ece9acaf2a7140b82e728361cdf24cf4e7d363
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2022-11-09 11:02:05 +0900

    #747

--- app/javascript/mastodon/features/getting_started/index.js
+++ app/javascript/mastodon/features/getting_started/index.js
@@ -105,7 +105,6 @@ class GettingStarted extends ImmutablePureComponent {
 
     navItems.push(
       <ColumnLink key='precure_timeline' icon='hashtag' text={intl.formatMessage(messages.precure_timeline)} to='/tags/precure_fun' />,
-      /*<ColumnLink key='community_timeline' icon='users' text={intl.formatMessage(messages.community_timeline)} to='/public/local' />,*/
       <ColumnLink key='public_timeline' icon='globe' text={intl.formatMessage(messages.public_timeline)} to='/public' />,
     );
 
