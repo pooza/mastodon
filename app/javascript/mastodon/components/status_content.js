76f885ce7a3d9e738e78aed3fa32de6c6dce1ec1
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2022-11-09 11:39:49 +0900

    #751

--- app/javascript/mastodon/components/status_content.js
+++ app/javascript/mastodon/components/status_content.js
@@ -8,7 +8,7 @@ import PollContainer from 'mastodon/containers/poll_container';
 import Icon from 'mastodon/components/icon';
 import { autoPlayGif, languages as preloadedLanguages, translationEnabled } from 'mastodon/initial_state';
 
-const MAX_HEIGHT = 202; // 20px * 10 (+ 2px padding at the top)
+const MAX_HEIGHT = 642; // 20px * 32 (+ 2px padding at the top)
 
 class TranslateButton extends React.PureComponent {
 
