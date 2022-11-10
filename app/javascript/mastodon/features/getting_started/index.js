b38627cae7a884c8a445128973d515612f424084
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2022-11-10 23:13:51 +0900

    #747

--- app/javascript/mastodon/features/getting_started/index.js
+++ app/javascript/mastodon/features/getting_started/index.js
@@ -125,7 +125,7 @@ class GettingStarted extends ImmutablePureComponent {
       for (let group of groups) {
         navItems.push(<ColumnSubheading key='header-{group.body}' text={group.body} />);
         for (let link of group.links) {
-          navItems.push(<ColumnLink key='{group.body}-{link.body}' icon={link.icon} text={link.body} to={link.href} />);
+          navItems.push(<ColumnLink key='{group.body}-{link.body}' icon={link.icon} text={link.body} href={link.href} />);
         }
       }
 
