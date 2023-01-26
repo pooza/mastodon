0b242d4c8bd11aed234b2192025a81f22b3d6f2f
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2023-01-27 01:24:31 +0900

    #786

--- app/javascript/mastodon/reducers/compose.js
+++ app/javascript/mastodon/reducers/compose.js
@@ -328,7 +328,7 @@ export default function compose(state = initialState, action) {
     case 'empty':
       return state.set('text', ['command: user_config', 'tagging:', '  user_tags: null'].join('\n'));
     case 'episodes':
-      return state;
+      return state.set('hoge', null);
     default:
       const createToot = name => {
         const request = new XMLHttpRequest();
