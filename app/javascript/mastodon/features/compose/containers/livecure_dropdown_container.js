5b4c38176de9a25ad431cba67b27fa8059ebc043
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2023-01-26 12:04:22 +0900

    #779

--- app/javascript/mastodon/features/compose/containers/livecure_dropdown_container.js
+++ app/javascript/mastodon/features/compose/containers/livecure_dropdown_container.js
@@ -1,18 +1,17 @@
 import { connect } from 'react-redux';
 import LivecureDropdown from '../components/livecure_dropdown';
-import { toggleLivecuresVisibility } from '../../../actions/compose';
+import { changeComposeVisibility } from '../../../actions/compose';
 import { openModal, closeModal } from '../../../actions/modal';
 import { isUserTouching } from '../../../is_mobile';
 
 const mapStateToProps = state => ({
-  isModalOpen: state.get('modal').modalType === 'ACTIONS',
-  value: '',
+  value: state.getIn(['compose', 'livecure']),
 });
 
 const mapDispatchToProps = dispatch => ({
 
   onChange (value) {
-    dispatch(toggleLivecuresVisibility(value));
+    dispatch(changeComposeVisibility(value));
   },
 
   isUserTouching,
--- app/javascript/mastodon/features/compose/containers/tagset_dropdown_container.js
+++ app/javascript/mastodon/features/compose/containers/tagset_dropdown_container.js
@@ -1,18 +1,17 @@
 import { connect } from 'react-redux';
 import TagsetDropdown from '../components/tagset_dropdown';
-import { changeTagset } from '../../../actions/compose';
+import { changeComposeVisibility } from '../../../actions/compose';
 import { openModal, closeModal } from '../../../actions/modal';
 import { isUserTouching } from '../../../is_mobile';
 
 const mapStateToProps = state => ({
-  isModalOpen: state.get('modal').modalType === 'ACTIONS',
-  value: '',
+  value: state.getIn(['compose', 'tagset']),
 });
 
 const mapDispatchToProps = dispatch => ({
 
   onChange (value) {
-    dispatch(changeTagset(value));
+    dispatch(changeComposeVisibility(value));
   },
 
   isUserTouching,
