2b579cc2001f5d0dab83f5b7d6ef62c1ee05c8aa
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2023-01-27 01:14:00 +0900

    #786

--- app/javascript/mastodon/reducers/compose.js
+++ app/javascript/mastodon/reducers/compose.js
@@ -328,8 +328,7 @@ export default function compose(state = initialState, action) {
     case 'empty':
       return state.set('text', ['command: user_config', 'tagging:', '  user_tags: null'].join('\n'));
     case 'episodes':
-      window.open('/mulukhiya/app/episode');
-      break;
+      return clearAll(state);
     default:
       const createToot = name => {
         const request = new XMLHttpRequest();
