ae1887ceb2418431515ae1c90bc749dd3eb7bbfd
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2023-09-05 16:48:12 +0900

    #808

--- app/javascript/mastodon/features/compose/components/tagset_dropdown.jsx
+++ app/javascript/mastodon/features/compose/components/tagset_dropdown.jsx
@@ -226,18 +226,18 @@ class TagsetDropdown extends PureComponent {
     const request = new XMLHttpRequest();
     request.open('GET', '/mulukhiya/api/program', false);
     request.send(null);
-    if (request.status !== 200) {return}
+    if (request.status !== 200) return;
     const result = JSON.parse(request.responseText);
     for (const k of Object.keys(result)) {
       const v = result[k];
-      if (!v.enable) {continue}
+      if (!v.enable) continue;
       const text = `「${v.series}」用タグセット`;
       const meta = [];
-      if (v.episode) {meta.push(`${v.episode}${v.episode_suffix || '話'}`)}
-      if (v.subtitle) {meta.push(`「${v.subtitle}」`)}
-      if (v.air) {meta.push('エア番組')}
-      if (v.livecure) {meta.push('実況')}
-      if (v.minutes) {meta.push(`(${v.minutes}分)`)}
+      if (v.episode) meta.push(`${v.episode}${v.episode_suffix || '話'}`);
+      if (v.subtitle) meta.push(`「${v.subtitle}」`);
+      if (v.air) meta.push('エア番組');
+      if (v.livecure) meta.push('実況');
+      if (v.minutes) meta.push(`(${v.minutes}分)`);
       v.extra_tags.map(tag => {meta.push(tag)});
       this.options.push({icon: 'hashtag', value: k, text: text, meta: meta.join(' ')});
     }
