7ca4aba7c72f8f6746b6b39166625d760477d315
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2024-10-02 07:42:39 +0900

    リアルタイムフィード リンク先修正

--- app/javascript/mastodon/features/firehose/index.jsx
+++ app/javascript/mastodon/features/firehose/index.jsx
@@ -166,7 +166,7 @@ const Firehose = ({ feedType, multiColumn }) => {
       </ColumnHeader>
 
       <div className='account__section-headline'>
-        <NavLink exact to='/tags/precure_fun'>
+        <NavLink exact to='/public/local'>
           <FormattedMessage tagName='div' id='firehose.local' defaultMessage='This community' />
         </NavLink>
 
