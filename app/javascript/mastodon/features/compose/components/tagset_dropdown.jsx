faa044a9bfa6fb2e647e0c1aa72bafda3c3fa037
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2024-10-02 08:59:14 +0900

    TagsetDropdown

--- app/javascript/mastodon/features/compose/components/tagset_dropdown.jsx
+++ app/javascript/mastodon/features/compose/components/tagset_dropdown.jsx
@@ -102,10 +102,10 @@ class TagsetDropdown extends PureComponent {
           if (v.livecure) meta.push('実況');
           if (v.minutes) meta.push(`(${v.minutes}分)`);
           v.extra_tags.map(tag => {meta.push(tag)});
-          this.options.push({icon: 'hashtag', iconComponent: TagIcon, value: k, text: text, meta: meta.join(' ')});
+          this.options.push({icon: 'tag', iconComponent: TagIcon, value: k, text: text, meta: meta.join(' ')});
         }
       }).then(_ => {
-        this.options.push({ icon: 'film', iconComponent: MovieIcon, value: 'episodes', text: formatMessage(messages.episodes_short), meta: formatMessage(messages.episodes_long) });
+        this.options.push({ icon: 'movie', iconComponent: MovieIcon, value: 'episodes', text: formatMessage(messages.episodes_short), meta: formatMessage(messages.episodes_long) });
       })
   }
 
