f1085c7589860b0961aa215b55ee1b6979c59a52
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2025-11-08 18:20:20 +0900

    update: ComposeForm レイアウト変更

--- app/javascript/mastodon/features/compose/components/language_dropdown.tsx
+++ app/javascript/mastodon/features/compose/components/language_dropdown.tsx
@@ -397,6 +397,7 @@ export const LanguageDropdown: React.FC = () => {
         })}
       >
         <Icon id='translate' icon={TranslateIcon} />
+        <span className='dropdown-button__label'>{current[2] ?? value}</span>
       </button>
 
       <Overlay
