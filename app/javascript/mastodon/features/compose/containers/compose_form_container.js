1d7c97660803627603a0378d5591d31f9b3b2d02
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2026-06-15 08:58:12 +0900

    fix: maxChars の Math.floor を復元
    4.6 マージ時に Math.floor が脱落し、サーバの max_characters 次第で
    maxChars が小数化し得る状態だった。原典どおり整数に丸める。

--- app/javascript/mastodon/features/compose/containers/compose_form_container.js
+++ app/javascript/mastodon/features/compose/containers/compose_form_container.js
@@ -58,7 +58,7 @@ const mapStateToProps = state => ({
     && !state.getIn(['settings', 'dismissed_banners', PRIVATE_QUOTE_MODAL_ID]),
   isInReply: state.getIn(['compose', 'in_reply_to']) !== null,
   lang: state.getIn(['compose', 'language']),
-  maxChars: state.getIn(['server', 'server', 'item', 'configuration', 'statuses', 'max_characters'], 3000) * 0.8,
+  maxChars: Math.floor(state.getIn(['server', 'server', 'item', 'configuration', 'statuses', 'max_characters'], 3000) * 0.8),
 });
 
 const mapDispatchToProps = (dispatch, props) => ({
